f2bfd478715d3c1b53f7e6ba971d8b07fd267256
Anthony Lock (1000051998) <LOCKAM1@student.op.ac.nz>
2020-08-03 14:32:07 +1200

    Arduino control from remote established

--- Remote_Control/Remote_Control.ino
+++ Remote_Control/Remote_Control.ino
@@ -12,23 +12,34 @@
 int ch1; // Here's where we'll keep our channel values
 int ch2;
 int ch3;
+int ch4;
+int led;
 
 void setup() {
 
-  pinMode(5, INPUT); // Set our input pins as such
-  pinMode(6, INPUT);
-  pinMode(7, INPUT);
-
-  Serial.begin(9600); // Pour a bowl of Serial
+  pinMode(A1, INPUT); // Set our input pins as such
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(A4, INPUT);
+  pinMode(A0, OUTPUT);
+  Serial.begin(9600); 
 
 }
 
 void loop() {
 
-  ch1 = pulseIn(5, HIGH, 25000); // Read the pulse width of 
-  ch2 = pulseIn(6, HIGH, 25000); // each channel
-  ch3 = pulseIn(7, HIGH, 25000);
-
+  ch1 = pulseIn(A1, HIGH); // Read the pulse width of 
+  ch2 = pulseIn(A2, HIGH); // each channel
+  ch3 = pulseIn(A3, HIGH);
+  ch4 = pulseIn(A4, HIGH);
+  
+  ch1 = (ch1 /100) - 9; //Rounding the channel input to a 0-10 scale for easy of use
+  ch2 = (ch2 /100) - 9;
+  ch3 = (ch3 /100) - 9;
+  ch4 = (ch4 /100) - 9;
+  
+  led = ch3 * 40; 
+  
   Serial.print("Channel 1:"); // Print the value of 
   Serial.println(ch1);        // each channel
 
@@ -37,7 +48,11 @@ void loop() {
 
   Serial.print("Channel 3:");
   Serial.println(ch3);
+  analogWrite(A0,led);
+  
+  Serial.print("Channel 4:");
+  Serial.println(ch4);
 
-  delay(100); // I put this here just to make the terminal 
+  delay(200); // I put this here just to make the terminal 
               // window happier
 }
